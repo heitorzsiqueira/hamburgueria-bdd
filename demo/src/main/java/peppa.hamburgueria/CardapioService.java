6497c368465aacbd2c3877e2edfe323e83a567a6
Heitor Zeferino Siqueira <111435132+heitorzsiqueira@users.noreply.github.com>
2025-11-24 08:26:53 -0300

    Add files via upload

---
+++ demo/src/main/java/peppa.hamburgueria/CardapioService.java
@@ -0,0 +1,15 @@
+public class CardapioService {
+   private final Map<String, Double> precos = new HashMap<>();
+
+    public void cadastrarItem(String nome, double preco) {
+        precos.put(nome, preco);
+    }
+
+    public boolean existe(String nome) {
+        return precos.containsKey(nome);
+    }
+
+    public double precoDe(String nome) {
+        return precos.getOrDefault(nome, 0.0);
+    }
+}
